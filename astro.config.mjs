4a81e3762f70de551f2bda247fe56b07b705be04
Marco Meyer <marcomeyer@posteo.de>
2024-06-06 21:14:37 +0200

    Update Astro! Update configuration to use 'astro:assets' for image imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
@@ -22,9 +21,9 @@ export default defineConfig({
         mdx(),
         sitemap(),
         tailwind(),
-        image({
+        /*         image({
             serviceEntryPoint: "@astrojs/image/sharp"
-        }),
+        }), */
         prefetch(),
         react()
     ]
