816081793f5b535fdd03fae51c02c5dd1966f2a5
Marco Meyer <marcomeyer@posteo.de>
2024-06-06 22:07:07 +0200

    chore: Update npm dependencies and Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,26 +5,25 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 //TODO: Set site to the actual domain !!!
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
 export default defineConfig({
-    site: "https://meyer-marco.github.io/",
-    vite: {
-        optimizeDeps: {
-            exclude: ["@resvg/resvg-js"]
-        },
-        ssr: {
-            external: ["svgo"]
-        }
+  site: "https://meyer-marco.github.io/",
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
     },
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind(),
-        /*         image({
-            serviceEntryPoint: "@astrojs/image/sharp"
-        }), */
-        prefetch(),
-        react()
-    ]
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  integrations: [mdx(), sitemap(), tailwind(),
+  /*         image({
+      serviceEntryPoint: "@astrojs/image/sharp"
+  }), */
+  prefetch(), react()],
+  output: "server",
+  adapter: netlify()
 });
