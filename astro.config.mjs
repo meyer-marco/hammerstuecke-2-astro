89418a29801c1959a6e25a9cca999e8ddaea1413
Marco Meyer <marcomeyer@posteo.de>
2024-06-13 14:30:21 +0200

    chore: Update Astro build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,28 @@
 import { defineConfig } from "astro/config";
+import path from "path";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
+import { fileURLToPath } from "url";
 
 //TODO: Set site to the actual domain !!!
-import netlify from "@astrojs/netlify";
-
-// https://astro.build/config
+//https://astro.build/config
+//const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
-  site: "https://meyer-marco.github.io/",
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
+    output: "static",
+    site: "https://meyer-marco.github.io/",
+    buildOptions: {
+        format: "file"
     },
-    ssr: {
-      external: ["svgo"]
-    }
-  },
-  integrations: [mdx(), sitemap(), tailwind(),
-  /*         image({
-      serviceEntryPoint: "@astrojs/image/sharp"
-  }), */
-  prefetch(), react()],
-  output: "server",
-  adapter: netlify()
+    vite: {
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"]
+        },
+        ssr: {
+            external: ["svgo"]
+        }
+    },
+    integrations: [mdx(), sitemap(), tailwind(), prefetch(), react()]
 });
