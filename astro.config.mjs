5c1479ab11d8b31861286de8e86a3d2c1df48b0e
Marco Meyer <marcomeyer@posteo.de>
2024-06-13 14:46:08 +0200

    chore: Update Astro build configuration and path for Astro project in deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import { fileURLToPath } from "url";
 //const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
     output: "static",
-    site: "https://meyer-marco.github.io/",
+    site: "https://meyer-marco.github.io/hammerstuecke-2-astro/",
     buildOptions: {
         format: "file"
     },
